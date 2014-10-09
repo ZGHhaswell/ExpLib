99ed3f934f170bd2fe739aa0de3de220edb9d5b9
ZGHhaswell <zhuguohao110@163.com>
2014-10-09 17:47:00 +0800

    加密

---
+++ DataEncrypt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEncrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IFormatProvider format = new System.Globalization.CultureInfo("zh-cn", true);
+
+            var content = DateTime.Now.ToString(format);
+            var code = EncryptService.DESEncrypt(content, "bejcxhds");
+            var content1 = EncryptService.DESDecrypt(code, "bejcxhds");
+            Console.WriteLine(content1);
+
+            Console.WriteLine(DateTime.Parse(content, format));
+        }
+    }
+}
